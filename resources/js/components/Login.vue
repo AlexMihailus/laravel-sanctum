f36e09278aa39595f733501a9333cb19976a31bb
Alex Mihailus <mihailus.A.G@gmail.com>
2022-04-07 23:06:02 +0300

    add getController, Login

--- resources/js/components/Login.vue
+++ resources/js/components/Login.vue
@@ -1,15 +1,48 @@
 <template>
-    <div>
-        Login
-    </div>
+  <div>
+    <input
+      v-model="email"
+      type="email"
+      placeholder="email"
+      class="form-control"
+    />
+    <input
+      v-model="password"
+      type="password"
+      placeholder="password"
+      class="form-control"
+    />
+    <input @click.prevent="login" type="submit" value="login" class="btn btn-primary" />
+  </div>
 </template>
 
 <script>
 export default {
-    name: "Login"
-}
+  name: "Login",
+
+  data() {
+    return {
+      email: null,
+      password: null,
+    }
+  },
+
+  methods: {
+    login() {
+      axios.get("/sanctum/csrf-cookie").then(response => {
+        axios
+          .post('/login', { email: this.email, password: this.password })
+          .then( r => {
+            console.log(r);
+          })
+          .catch( err => {
+              console.log(err.response)
+          })
+      })
+    }
+  }
+};
 </script>
 
 <style lang="scss" scoped>
-
 </style>
